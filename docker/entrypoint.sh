844e9d4365888f0c79b40a3bcc8864b1502f053d
richard morris <richard@corkmate.com>
2023-07-05 17:29:01 +0000

    run ssh-agent and use python3.11 explicitly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,9 @@ SSH_PRIVATE_KEY=/root/.ssh/id_rsa
 if [ -f "${SSH_PRIVATE_KEY}" ]; then
     # Set permissions
     chmod 600 "${SSH_PRIVATE_KEY}"
+    # make sure that agent is running and has the key
+    eval "$(ssh-agent -s)"
+    ssh-add /root/.ssh/id_rsa
 else
     echo "No SSH private key found at: ${SSH_PRIVATE_KEY}"
     echo "If you don't mount your SSH private key when you run the container, you won't be able to access private repositories on github."
